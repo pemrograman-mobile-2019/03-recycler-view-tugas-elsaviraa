348166323b4ea9bf5613b4125e3c4f4da612651f
elsaviraa <elsavira33@gmail.com>
2019-02-27 20:27:24 +0700

    universitasAdapter-2

--- app/src/main/java/com/example/tugas_recylerview/adapter/UniversitasAdapter.java
+++ app/src/main/java/com/example/tugas_recylerview/adapter/UniversitasAdapter.java
@@ -34,4 +34,37 @@ public class UniversitasAdapter extends RecyclerView.Adapter<UniversitasAdapter.
         return new ViewHolder(filmView);
     }
 
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Universitas universitas = universitasList.get(i);
+
+        if (universitas.getImg() != null) {
+            Picasso.get().load(universitas.getImg()).into(viewHolder.imageView);
+        } else {
+            Picasso.get().load(universitas.getImg_local()).into(viewHolder.imageView);
+        }
+
+        viewHolder.textView.setText(universitas.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return universitasList.size();
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView textView;
+        TextView descView;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.item_universitas_img_local);
+            textView = itemView.findViewById(R.id.item_universitas_title);
+            descView = itemView.findViewById(R.id.item_universitas_desc);
+        }
+    }
+
 }
