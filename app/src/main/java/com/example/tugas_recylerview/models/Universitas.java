0ea7f0058086c6143680872492a6147c2a3953db
elsaviraa <elsavira33@gmail.com>
2019-02-27 19:22:50 +0700

    Menambahkan universitas.java

--- app/src/main/java/com/example/tugas_recylerview/models/Universitas.java
+++ app/src/main/java/com/example/tugas_recylerview/models/Universitas.java
@@ -1,6 +1,79 @@
 package com.example.tugas_recylerview.models;
 
 public class Universitas {
+    private String id;
+    private String title;
+    private String desc;
+    private String img;
+    private int img_local;
 
+    public Universitas(String id, String title, String desc, String img) {
+        this.id = id;
+        this.title = title;
+        this.desc = desc;
+        this.img = img;
+    }
 
+    public Universitas(String id, String title, String desc, int img_local) {
+        this.id = id;
+        this.title = title;
+        this.desc = desc;
+        this.img_local = img_local;
+    }
+
+    public Universitas(String id, String title, String desc, int img_local) {
+        this.id = id;
+        this.title = title;
+        this.desc = desc;
+        this.img_local = img_local;
+    }
+
+    public String getId() {
+
+        return id;
+    }
+
+    public void setId(String id) {
+
+        this.id = id;
+    }
+
+    public String getTitle() {
+
+        return title;
+    }
+
+    public void setTitle(String title) {
+
+        this.title = title;
+    }
+
+    public String getImg() {
+
+        return img;
+    }
+
+    public void setImg(String img) {
+
+        this.img = img;
+    }
+
+    public int getImg_local() {
+
+        return img_local;
+    }
+
+    public void setImg_local(int img_local) {
+
+        this.img_local = img_local;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+
+        this.desc = desc;
+    }
 }
